4d7dac2160276077a6a6650f02b0577f606f8110
urosrac <uros.rac@gmail.com>
2020-03-31 21:45:01 +0200

    Added quantity

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 db.create_all()
 @app.route("/")
 def index():
-    ListOfFoodItems = db.query(FoodItems).filter_by(IsExpired=0, IsDeleted=0, IsRemoved=0).order_by(FoodItems.DatumPoteka.asc()).all()
+    ListOfFoodItems = db.query(FoodItems).filter_by(IsExpired=0, IsRemoved=0).filter(FoodItems.Kolicina > 0).order_by(FoodItems.DatumPoteka.asc()).all()
     i, l = 0, len(ListOfFoodItems)
     while i < l:
         if ListOfFoodItems[i].DatumPoteka < datetime.now() - timedelta(days = 1):
@@ -29,34 +29,36 @@ def AddedFoodItem():
     FoodItem = FoodItems(ImeZivila = request.form.get("FoodName"),
                          DatumVpisa = datetime.now(),
                          DatumPoteka = datetime.strptime(request.form.get("ExpirationDate"), '%Y-%m-%d'),
+                         Kolicina = request.form.get("Quantity"),
                          IsExpired = 0,
-                         IsDeleted = 0,
                          IsRemoved = 0)
     db.add(FoodItem)
     db.commit()
     return redirect(url_for("AddFood"))
 
-@app.route("/DeleteFoodItem", methods = ["GET","POST"])
-def DeletedFoodItem():
-    FoodItemID = request.form.get("FoodItemID")
-    FoodItem = db.query(FoodItems).get(FoodItemID)
-    FoodItem.IsDeleted = 1
+@app.route("/AddDeleteFoodItem", methods = ["GET","POST"])
+def AddDeleteFoodItem():
+    FoodItem = db.query(FoodItems).get(request.form.get("FoodItemID"))
+    if request.form["SubmitButton"] == "+":
+        FoodItem.Kolicina += 1
+    else:
+        FoodItem.Kolicina -= 1
     db.commit()
     return redirect(url_for("index"))
 
 @app.route("/UrejanjePoteklih")
-def ExpiredFood():
-    ListOfExpiredOrDeletedFoodItems = db.query(FoodItems).filter_by(IsExpired = 0, IsDeleted = 1, IsRemoved = 0).order_by(FoodItems.DatumPoteka.asc()).all()
-    i, l = 0, len(ListOfExpiredOrDeletedFoodItems)
+def DeletedFood():
+    ListOfDeletedFoodItems = db.query(FoodItems).filter_by(IsExpired = 0, IsRemoved = 0, Kolicina = 0).order_by(FoodItems.DatumPoteka.asc()).all()
+    i, l = 0, len(ListOfDeletedFoodItems)
     while i < l:
-        if ListOfExpiredOrDeletedFoodItems[i].DatumPoteka < datetime.now() - timedelta(days = 1):
-            el = ListOfExpiredOrDeletedFoodItems.pop(i)
+        if ListOfDeletedFoodItems[i].DatumPoteka < datetime.now() - timedelta(days = 1):
+            el = ListOfDeletedFoodItems.pop(i)
             el.IsExpired = 1
             db.commit()
             l -= 1
             i -= 1
         i += 1
-    return render_template("ExpiredFood.html", ListOfExpiredOrDeletedFoodItems = ListOfExpiredOrDeletedFoodItems,
+    return render_template("ExpiredFood.html", ListOfDeletedFoodItems = ListOfDeletedFoodItems,
                            CurrentDate0 = datetime.now() + timedelta(days = 2),
                            CurrentDate1 = datetime.now() + timedelta(days = 5),
                            CurrentDate2 = datetime.now() + timedelta(days = 10))
@@ -67,9 +69,9 @@ def DeleteOrRestoreFoodItem():
     if request.form["SubmitButton"] == "X":
         FoodItem.IsRemoved = 1
     else:
-        FoodItem.IsDeleted = 0
+        FoodItem.Kolicina += 1
     db.commit()
-    return redirect(url_for("ExpiredFood"))
+    return redirect(url_for("DeletedFood"))
 
 if __name__ == '__main__':
     app.run()
